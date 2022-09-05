b2b353a8c4ae0dd3228e47d61f1752e33377d4ff
“Akpughe” <davidakpughe2@gmail.com>
2022-09-06 01:56:18 +0200

    add - scroll event listener

--- components/Search/index.js
+++ components/Search/index.js
@@ -1,37 +1,53 @@
-import React, {useState} from 'react'
-import { Switch } from '@headlessui/react'
+import React, { useState, useEffect } from 'react';
+import { Switch } from '@headlessui/react';
 
-const Search = ({handleSearch, value, onChange, loading, enabled, setEnabled}) => {
-  
-  console.log('enabled', enabled)
+const Search = ({
+  handleSearch,
+  value,
+  onChange,
+  loading,
+  enabled,
+  setEnabled,
+}) => {
+  console.log('enabled', enabled);
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 50) {
+        return document.querySelector('.searchbar').classList.add('show');
+      }
+      return document.querySelector('.searchbar').classList.remove('show');
+    });
+  });
   return (
-    <div className="flex items-center">
+    <div className="searchbar flex items-center w-full bg-white">
       <form
-      onSubmit={handleSearch}
+        onSubmit={handleSearch}
         className="flex sm:space-x-4 space-x-2 items-center sm:px-10 px-5 w-full"
       >
         <div className="sm:w-2/5 w-full">
-          <input value={value} onChange={onChange} loading={loading} type="text" placeholder="Enter an Address, City or ZIP code" />
+          <input
+            value={value}
+            onChange={onChange}
+            loading={loading}
+            type="text"
+            placeholder="Enter an Address, City or ZIP code"
+          />
         </div>
         <div>
-          <div
-            className="sm:flex hidden space-x-2 w-full bg-[#f3f3f3] py-3 px-4 rounded-md-2 text-sm"
-          >
+          <div className="sm:flex hidden space-x-2 w-full bg-[#f3f3f3] py-3 px-4 rounded-md-2 text-sm">
             <div>For sale</div>
             <div>•</div>
           </div>
         </div>
         <div>
-          <div
-            className="sm:flex hidden space-x-2 w-full bg-[#f3f3f3] py-3 px-4 rounded-md-2 text-sm"
-          >
+          <div className="sm:flex hidden space-x-2 w-full bg-[#f3f3f3] py-3 px-4 rounded-md-2 text-sm">
             <div>Type: House</div>
             <div>•</div>
           </div>
         </div>
         <div>
           <button
-          type='submit'
+            type="submit"
             className="py-2.5 px-4 bg-blue-500 rounded-md-2 capitalize text-white font-semibold"
           >
             search
@@ -39,25 +55,25 @@ const Search = ({handleSearch, value, onChange, loading, enabled, setEnabled}) =
         </div>
       </form>
 
-      <div className="w-full text-sm sm:flex items-center hidden"><Switch
-      checked={enabled}
-      onChange={setEnabled}
-      className={`${
-        enabled ? 'bg-blue-600' : 'bg-gray-200'
-      } relative inline-flex h-6 w-11 items-center rounded-full`}
-    >
-      <span className="sr-only">Enable notifications</span>
-      <span
-        className={`${
-          enabled ? 'translate-x-6' : 'translate-x-1'
-        } inline-block h-4 w-4 transform rounded-full bg-white`}
-      />
-      
-    </Switch>
-    <span className="text-xs ml-2 font-semibold">Map view</span></div>
-    
+      <div className="w-full text-sm sm:flex items-center hidden">
+        <Switch
+          checked={enabled}
+          onChange={setEnabled}
+          className={`${
+            enabled ? 'bg-blue-600' : 'bg-gray-200'
+          } relative inline-flex h-6 w-11 items-center rounded-full`}
+        >
+          <span className="sr-only">Enable notifications</span>
+          <span
+            className={`${
+              enabled ? 'translate-x-6' : 'translate-x-1'
+            } inline-block h-4 w-4 transform rounded-full bg-white`}
+          />
+        </Switch>
+        <span className="text-xs ml-2 font-semibold">Map view</span>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
