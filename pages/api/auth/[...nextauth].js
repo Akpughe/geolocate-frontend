90731620c95314aa3ef14127e0df1f1efb3aafd3
“Akpughe” <davidakpughe2@gmail.com>
2022-10-06 00:55:19 +0100

    change auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,11 +1,16 @@
 import NextAuth from 'next-auth';
 import FacebookProvider from 'next-auth/providers/facebook';
+import GoogleProvider from "next-auth/providers/google";
 
 export default NextAuth({
   providers: [
-    FacebookProvider({
-      clientId: '1100330467251979',
-      clientSecret: 'bde426f94daa87c6dd1a29f80462b0fa',
+    // FacebookProvider({
+    //   clientId: '1100330467251979',
+    //   clientSecret: 'bde426f94daa87c6dd1a29f80462b0fa',
+    // }),
+    GoogleProvider({
+      clientId: '714125491832-lpqc6hlqad18cf4p3fes7jirhas8clc3.apps.googleusercontent.com',
+      clientSecret: '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',
     }),
   ],
 });
